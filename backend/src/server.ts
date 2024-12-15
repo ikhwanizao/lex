232ad1c0ae5dda8b7e17927e9ef8e69bf6ee07fd
ikhwanizao <ikhwanm32@gmail.com>
2024-12-15 23:46:43 +0800

    Configure CORS to allow specific origins and enable credentials

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,10 @@ const start = async () => {
     const { admin, router } = initAdmin(db)
 
     app.use(admin.options.rootPath, router)
-    app.use(cors())
+    app.use(cors({
+        origin: ['http://13.212.250.63', 'http://localhost:5173'],
+        credentials: true
+    }));
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
 
